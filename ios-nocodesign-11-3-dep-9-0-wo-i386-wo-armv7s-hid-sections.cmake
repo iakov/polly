aa17cd2b4c82a990d3e5d9d7c05740ec8b2810d9
iakov <jake.kirilenko@gmail.com>
2018-05-03 10:05:49 +0300

    ios-nocodesign-11-3-dep-9-0-wo-i386-wo-armv7s-hid-sections

---
+++ ios-nocodesign-11-3-dep-9-0-wo-i386-wo-armv7s-hid-sections.cmake
@@ -0,0 +1,44 @@
+# Copyright (c) 2017-2018, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED POLLY_IOS_NOCODESIGN_11_3_DEP_9_3_CMAKE_)
+  return()
+else()
+  set(POLLY_IOS_NOCODESIGN_11_3_DEP_9_3_CMAKE_ 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+set(IOS_SDK_VERSION 11.3)
+set(IOS_DEPLOYMENT_SDK_VERSION 9.0)
+
+set(POLLY_XCODE_COMPILER "clang")
+polly_init(
+    "iOS ${IOS_SDK_VERSION} / Deployment ${IOS_DEPLOYMENT_SDK_VERSION} / Universal wo i386 & armv7s (iphoneos + iphonesimulator) / \
+${POLLY_XCODE_COMPILER} / \
+No code sign / \
+c++14 support"
+    "Xcode"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+
+include(polly_fatal_error)
+
+# Fix try_compile
+include(polly_ios_bundle_identifier)
+
+set(CMAKE_MACOSX_BUNDLE YES)
+
+include("${CMAKE_CURRENT_LIST_DIR}/flags/ios_nocodesign.cmake")
+
+set(IPHONEOS_ARCHS armv7;arm64)
+set(IPHONESIMULATOR_ARCHS x86_64)
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/xcode.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/os/iphone.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx14.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/hidden.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/flags/function-sections.cmake")
