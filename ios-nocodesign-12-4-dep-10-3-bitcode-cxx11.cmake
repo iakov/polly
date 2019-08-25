c7da7bb9b5b0ae477754608df520d0bc6415f733
iakov <jake.kirilenko@gmail.com>
2019-08-25 10:54:10 +0300

    ios-nocodesign-12-4-dep-10-3-bitcode-cxx11

--- ios-nocodesign-12-4-dep-10-3-bitcode-cxx11.cmake
+++ ios-nocodesign-12-4-dep-10-3-bitcode-cxx11.cmake
@@ -1,18 +1,18 @@
 # Copyright (c) 2017, Ruslan Baratov
 # All rights reserved.
 
-if(DEFINED POLLY_IOS_NOCODESIGN_11_4_DEP_9_0_BITCODE_CMAKE_)
+if(DEFINED POLLY_IOS_NOCODESIGN_12_4_DEP_10_3_BITCODE_CMAKE_)
   return()
 else()
-  set(POLLY_IOS_NOCODESIGN_11_4_DEP_9_0_BITCODE_CMAKE_ 1)
+  set(POLLY_IOS_NOCODESIGN_12_4_DEP_10_3_BITCODE_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_clear_environment_variables.cmake")
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
-set(IOS_SDK_VERSION 11.4)
-set(IOS_DEPLOYMENT_SDK_VERSION 9.0)
+set(IOS_SDK_VERSION 12.4)
+set(IOS_DEPLOYMENT_SDK_VERSION 10.3)
 
 set(POLLY_XCODE_COMPILER "clang")
 polly_init(
